26d5299d8fa7c688ecdaa9b31c80064e4db804d0
shayashi <shayashi@redhat.com>
2021-03-12 14:36:53 +0900

    First imprement.

--- bin/setup_jenkins.sh
+++ bin/setup_jenkins.sh
@@ -17,12 +17,25 @@ echo "Setting up Jenkins in project ${GUID}-jenkins from Git Repo ${REPO} for Cl
 # You do not want to set up things in the wrong project.
 
 # TBD
+oc new-project ${GUID}-jenkins --display-name "${GUID} Shared Jenkins"
+oc new-app jenkins-persistent --param ENABLE_OAUTH=true --param MEMORY_LIMIT=2Gi --param VOLUME_CAPACITY=4Gi --param DISABLE_ADMINISTRATIVE_MONITORS=true --as-deployment-config=true -n ${GUID}-jenkins
 
+oc set resources dc jenkins --limits=memory=2Gi,cpu=2 --requests=memory=1Gi,cpu=500m -n ${GUID}-jenkins
 
 # Create custom agent container image with skopeo.
 # Build config must be called 'jenkins-agent-appdev' for the test below to succeed
 
 # TBD
+oc new-build --strategy=docker -D $'FROM registry.access.redhat.com/ubi8/go-toolset:latest as builder\n
+ENV SKOPEO_VERSION=v1.0.0\n
+RUN git clone -b $SKOPEO_VERSION https://github.com/containers/skopeo.git && cd skopeo/ && make binary-local DISABLE_CGO=1\n
+FROM image-registry.openshift-image-registry.svc:5000/openshift/jenkins-agent-maven:v4.0 as final\n
+USER root\n
+RUN mkdir /etc/containers\n
+COPY --from=builder /opt/app-root/src/skopeo/default-policy.json /etc/containers/policy.json\n
+COPY --from=builder /opt/app-root/src/skopeo/skopeo /usr/bin\n
+USER 1001' --name=jenkins-agent-appdev -n ${GUID}-jenkins
+
 
 
 # Create Secret with credentials to access the private repository
@@ -31,7 +44,7 @@ echo "Setting up Jenkins in project ${GUID}-jenkins from Git Repo ${REPO} for Cl
 # Passing it from Jenkins would show it in the Jenkins Log
 
 # TBD
-
+oc create secret generic private-repo-secret --from-literal=username=shayashi-redhat.com --from-literal=password=zk5VLQS25EMurb9 -n ${GUID}-jenkins
 
 
 # Create pipeline build config pointing to the ${REPO} with contextDir `openshift-tasks`
@@ -39,8 +52,22 @@ echo "Setting up Jenkins in project ${GUID}-jenkins from Git Repo ${REPO} for Cl
 # Make sure you use your secret to access the repository
 
 # TBD
+echo 'apiVersion: v1
+kind: "BuildConfig"
+metadata:
+    name: "tasks-pipeline"
+spec:
+    source:
+      type: "Git"
+      git:
+        uri: "${REPO}"
+      contextDir: "openshift-tasks"
+    strategy:
+      type: "JenkinsPipeline"
+      jenkinsPipelineStrategy:
+        jenkinsfilePath: Jenkinsfile' | oc apply -f -n ${GUID}-jenkins
 
-
+oc set build-secret --source bc/tasks-pipeline private-repo-secret -n ${GUID}-jenkins
 
 # Set up ConfigMap with Jenkins Agent definition
 oc create -f ./manifests/agent-cm.yaml -n ${GUID}-jenkins
