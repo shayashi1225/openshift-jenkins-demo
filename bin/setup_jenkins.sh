872e4b974836189dece2f5af7abdc8d2d9b2900f
shayashi <shayashi@redhat.com>
2021-03-12 14:48:16 +0900

    Updated.

--- bin/setup_jenkins.sh
+++ bin/setup_jenkins.sh
@@ -17,7 +17,6 @@ echo "Setting up Jenkins in project ${GUID}-jenkins from Git Repo ${REPO} for Cl
 # You do not want to set up things in the wrong project.
 
 # TBD
-oc new-project ${GUID}-jenkins --display-name "${GUID} Shared Jenkins"
 oc new-app jenkins-persistent --param ENABLE_OAUTH=true --param MEMORY_LIMIT=2Gi --param VOLUME_CAPACITY=4Gi --param DISABLE_ADMINISTRATIVE_MONITORS=true --as-deployment-config=true -n ${GUID}-jenkins
 
 oc set resources dc jenkins --limits=memory=2Gi,cpu=2 --requests=memory=1Gi,cpu=500m -n ${GUID}-jenkins
