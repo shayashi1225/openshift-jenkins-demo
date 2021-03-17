c25c4621a5f8b46b998028fe0868cbd1dba7c595
shayashi <shayashi@redhat.com>
2021-03-17 17:30:03 +0900

    Updated

--- bin/cleanup.sh
+++ bin/cleanup.sh
@@ -8,6 +8,6 @@ fi
 
 GUID=$1
 echo "Removing all Homework Projects for GUID=$GUID"
-oc delete project $GUID-jenkins
+#oc delete project $GUID-jenkins
 oc delete project $GUID-tasks-dev
 oc delete project $GUID-tasks-prod
